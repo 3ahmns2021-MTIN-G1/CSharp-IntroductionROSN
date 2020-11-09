2147f3a6a66fcfe2005a79148cb1ee75732c7c58
NadineRosenzopf <nadine.rosenzopf02@htl-salzburg.ac.at>
2020-11-09 14:22:26 +0100

    Des sind die Beschreibungen für Klasse etc

---
+++ Assets/Srcipts/ExampleClass.cs
@@ -0,0 +1,20 @@
+﻿//Bibliotheken/Asembly Referenz/Namespaces
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//Deklaration einer Klasse
+
+    //public         -> acess modifier (Zugriffsreche / Keyword (vorreservierte )
+    // class         -> keyword zur Deklaration einer Klasse
+    // ExampleClass  -> identifier (Name der Klasse)
+    // :MonoBehaviour->Erstellen eine Unterklasse von Monobehaviour
+    // {}            -> Scope/Wirkungsbereich (Was ist alles in der Klasse erhalten)
+
+public class ExampleClass : MonoBehaviour
+{
+   
+    {
+        
+    }
+}
